3a2e1f0f370b35dff0b745e969aa1a00ec0646ca
Steven Zawaski <5233295+szawaski@users.noreply.github.com>
2021-12-27 20:00:13 -0700

    Identity - Constructors for net48

--- Framework/Zerra.Identity/IdentityHttpRequest.cs
+++ Framework/Zerra.Identity/IdentityHttpRequest.cs
@@ -1,31 +1,39 @@
 ﻿#if !NET48
 using Microsoft.AspNetCore.Http;
+using System.Linq;
 #endif
 
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Zerra.Identity
 {
     public class IdentityHttpRequest
     {
-        public IReadOnlyDictionary<string, string> Query { get; private set; }
         public string QueryString { get; private set; }
+        public IReadOnlyDictionary<string, string> Query { get; private set; }
 
         public bool HasFormContentType { get; private set; }
         public IReadOnlyDictionary<string, string> Form { get; private set; }
 
+        public IdentityHttpRequest(string queryString, Dictionary<string, string> query, bool hasFormContentType, Dictionary<string, string> form)
+        {
+            this.QueryString = queryString;
+            this.Query = query;
+            this.HasFormContentType = form != null;
+            this.HasFormContentType = hasFormContentType;
+            this.Form = form;
+        }
+
 #if !NET48
         public IdentityHttpRequest(HttpContext context)
         {
-            this.Query = context.Request.Query.ToDictionary(x => x.Key, x => (string)x.Value);
             this.QueryString = context.Request.QueryString.Value;
+            this.Query = context.Request.Query.ToDictionary(x => x.Key, x => (string)x.Value);
 
             this.HasFormContentType = context.Request.HasFormContentType;
             if (this.HasFormContentType)
                 this.Form = context.Request.Form.ToDictionary(x => x.Key, x => (string)x.Value);
         }
 #endif
-
     }
 }
