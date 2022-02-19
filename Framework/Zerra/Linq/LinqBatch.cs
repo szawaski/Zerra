7fe0b33e9d9423c08e0b6f8841e4cad5ecb31856
Steven Zawaski <5233295+szawaski@users.noreply.github.com>
2022-02-19 14:19:55 -0700

    Linq Batch Extensions

---
+++ Framework/Zerra/Linq/LinqBatch.cs
@@ -0,0 +1,90 @@
+﻿// Copyright © KaKush LLC
+// Written By Steven Zawaski
+// Licensed to you under the MIT license
+
+using System;
+using System.Collections.Generic;
+
+namespace Zerra.Linq
+{
+    public static class LinqBatchExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> source, int size)
+        {
+            var buffer = new T[size];
+            var count = 0;
+
+            foreach (var item in source)
+            {
+                buffer[count++] = item;
+
+                if (count != size)
+                    continue;
+
+                yield return buffer;
+
+                count = 0;
+            }
+
+            if (count > 0)
+            {
+                Array.Resize(ref buffer, count);
+                yield return buffer;
+            }
+        }
+
+        public static IEnumerable<ICollection<T>> Batch<T>(this ICollection<T> source, int size)
+        {
+            if (source.Count == 0)
+                yield break;
+            if (source.Count <= size)
+            {
+                yield return source;
+                yield break;
+            }
+
+            var buffer = new T[size];
+            var count = 0;
+
+            foreach (var item in source)
+            {
+                buffer[count++] = item;
+
+                if (count != size)
+                    continue;
+
+                yield return buffer;
+
+                count = 0;
+            }
+
+            if (count > 0)
+            {
+                Array.Resize(ref buffer, count);
+                yield return buffer;
+            }
+        }
+
+        public static IEnumerable<T[]> Batch<T>(this T[] source, int size)
+        {
+            if (source.Length == 0)
+                yield break;
+            if (source.Length <= size)
+            {
+                yield return source;
+                yield break;
+            }
+
+            var buffer = new T[size];
+
+            for (var i = 0; i < source.Length; i += size)
+            {
+                var blockSize = Math.Min(source.Length - i, size);
+                Array.Copy(source, i, buffer, 0, blockSize);
+                if (blockSize < size)
+                    Array.Resize(ref buffer, blockSize);
+                yield return buffer;
+            }
+        }
+    }
+}
