016ce6475e9cfe16f01432ce69dabf8bf8d46e39
Steven Zawaski <5233295+szawaski@users.noreply.github.com>
2022-04-27 13:27:41 -0600

    Map Updates

---
+++ Framework/Zerra/Map/IMapLogger.cs
@@ -0,0 +1,13 @@
+﻿// Copyright © KaKush LLC
+// Written By Steven Zawaski
+// Licensed to you under the MIT license
+
+namespace Zerra
+{
+    public interface IMapLogger
+    {
+        void LogPropertyChange(string source, string sourceValue, string target, string targetValue);
+        void LogPropertyNoChange(string source, string target, string value);
+        void LogNewObject(string source, string target, string type);
+    }
+}
