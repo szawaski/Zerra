5421516289b578206dbff2ab852fde9ad5d6c564
Steven Zawaski <5233295+szawaski@users.noreply.github.com>
2022-06-08 20:26:06 -0600

    Move Password Generation

--- Framework/Zerra/Encryption/Hasher.cs
+++ Framework/Zerra/Encryption/Hasher.cs
@@ -23,48 +23,6 @@ namespace Zerra.Encryption
             }
         }
 
-        private static readonly char[] passwordCharactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXUYZ".ToCharArray();
-        private static readonly char[] passwordCharactersLower = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-        private static readonly char[] passwordCharactersNumeric = "0123456789".ToCharArray();
-        private static readonly char[] passwordCharactersSpecial = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".ToCharArray(); //OWASP
-        public static string GeneratePassword(int length, bool upperCase, bool lowerCase, bool numeric, bool special)
-        {
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                var passwordChars = new List<char>();
-                if (upperCase)
-                    passwordChars.AddRange(passwordCharactersUpper);
-                if (lowerCase)
-                    passwordChars.AddRange(passwordCharactersLower);
-                if (numeric)
-                    passwordChars.AddRange(passwordCharactersNumeric);
-                if (special)
-                    passwordChars.AddRange(passwordCharactersSpecial);
-
-                var chars = new char[length];
-                for (int x = 0; x < length; x++)
-                {
-                    var random = GetRandomNumber(rng, 0, passwordChars.Count);
-                    chars[x] = passwordChars[random];
-                }
-                return new String(chars);
-            }
-        }
-
-        public static int GetRandomNumber(RandomNumberGenerator rng, int minValue, int maxValue)
-        {
-            if (minValue > maxValue)
-                throw new ArgumentOutOfRangeException(nameof(minValue));
-            if (minValue == maxValue)
-                return minValue;
-            long diff = maxValue - minValue;
-            var intBuffer = new byte[4];
-
-            rng.GetBytes(intBuffer);
-            var rand = BitConverter.ToUInt32(intBuffer, 0);
-            return (int)Math.Round(((rand / (double)uint.MaxValue) * diff) + minValue);
-        }
-
         private static HashAlgorithm GetHashAlgorithm(HashAlgoritmType hashAlgoritmType)
         {
             return hashAlgoritmType switch
---
+++ Framework/Zerra/Encryption/Password.cs
@@ -0,0 +1,55 @@
+﻿// Copyright © KaKush LLC
+// Written By Steven Zawaski
+// Licensed to you under the MIT license
+
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Zerra.Encryption
+{
+    public static class Password
+    {
+        private static readonly char[] passwordCharactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXUYZ".ToCharArray();
+        private static readonly char[] passwordCharactersLower = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        private static readonly char[] passwordCharactersNumeric = "0123456789".ToCharArray();
+        private static readonly char[] passwordCharactersSpecial = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".ToCharArray(); //OWASP
+        public static string GeneratePassword(int length, bool upperCase, bool lowerCase, bool numeric, bool owaspSpecialCharacters)
+        {
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                var passwordChars = new List<char>();
+                if (upperCase)
+                    passwordChars.AddRange(passwordCharactersUpper);
+                if (lowerCase)
+                    passwordChars.AddRange(passwordCharactersLower);
+                if (numeric)
+                    passwordChars.AddRange(passwordCharactersNumeric);
+                if (owaspSpecialCharacters)
+                    passwordChars.AddRange(passwordCharactersSpecial);
+
+                var chars = new char[length];
+                for (int x = 0; x < length; x++)
+                {
+                    var random = GetRandomNumber(rng, 0, passwordChars.Count);
+                    chars[x] = passwordChars[random];
+                }
+                return new String(chars);
+            }
+        }
+
+        public static int GetRandomNumber(RandomNumberGenerator rng, int minValue, int maxValue)
+        {
+            if (minValue > maxValue)
+                throw new ArgumentOutOfRangeException(nameof(minValue));
+            if (minValue == maxValue)
+                return minValue;
+            long diff = maxValue - minValue;
+            var intBuffer = new byte[4];
+
+            rng.GetBytes(intBuffer);
+            var rand = BitConverter.ToUInt32(intBuffer, 0);
+            return (int)Math.Round(((rand / (double)uint.MaxValue) * diff) + minValue);
+        }
+    }
+}
