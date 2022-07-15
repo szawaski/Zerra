4f67115d6a0c6e3f01a9be4e07d0003719eb973d
Steven Zawaski <5233295+szawaski@users.noreply.github.com>
2022-07-14 20:07:04 -0600

    Testing Messaging Services

--- Demo/ZerraDemo.Common/ServiceManager.cs
+++ Demo/ZerraDemo.Common/ServiceManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Zerra;
 using Zerra.CQRS;
+using Zerra.CQRS.AzureEventHub;
+using Zerra.CQRS.AzureServiceBus;
 using Zerra.CQRS.Settings;
 using Zerra.Logger;
 
@@ -10,11 +12,9 @@ namespace ZerraDemo.Common
     {
         public static void StartServices()
         {
-            var entryAssembly = Config.EntryAssembly;
-            if (entryAssembly == null)
+            var settingsName = Config.EntryAssemblyName;
+            if (settingsName == null)
                 throw new Exception($"Entry Assembly is null, {nameof(ServiceManager)} cannot identify which service is running");
-                    
-            var settingsName = entryAssembly.GetName().Name;
 
             var serviceSettings = CQRSSettings.Get();
 
@@ -28,7 +28,7 @@ namespace ZerraDemo.Common
             //----------------------------------------------------------
 
             //Option1A: Enable this for Tcp for backend only services
-            var serviceCreator = serviceCreatorInternal;
+            //var serviceCreator = serviceCreatorInternal;
 
             //Option1B: Enable this for Http which can be access directly from a front end
             //var authorizor = new DemoCookieApiAuthorizer();
@@ -40,13 +40,17 @@ namespace ZerraDemo.Common
             //Option1D: Enable this using Kafka for event streaming commands/events
             //var serviceCreator = new KafkaServiceCreator(serviceSettings.MessageHost, serviceCreatorInternal);
 
+            //Option1E: Enable this using Azure Event Hub for event streaming commands/events
+            //var serviceCreator = new AzureEventHubServiceCreator(serviceSettings.MessageHost, serviceCreatorInternal);
 
+            //Option1F: Enable this using Azure Event Hub for event streaming commands/events
+            var serviceCreator = new AzureServiceBusServiceCreator(serviceSettings.MessageHost, serviceCreatorInternal);
 
             //Enable one of the following routing options
             //----------------------------------------------------------
 
             //Option2A: Enable this for direct service communication, no custom relay/loadbalancer (can still use container balancers)
-            Bus.StartServices(settingsName, serviceSettings, serviceCreator, null);
+            Bus.StartServices(settingsName, serviceSettings, serviceCreator);
 
             //Option2B: Enable this to use the relay/loadbalancer
             //var relayRegister = new RelayRegister(serviceSettings.RelayUrl, serviceSettings.RelayKey);
