dfe326b24c2485157c0e4c0e3f7213b0ae31e8cf
Steven Zawaski <5233295+szawaski@users.noreply.github.com>
2023-09-11 16:32:54 -0600

    Service Log Attribute

--- Demo/ZerraDemo.Domain/Pets/IPetsQueryProvider.cs
+++ Demo/ZerraDemo.Domain/Pets/IPetsQueryProvider.cs
@@ -7,16 +7,12 @@ using ZerraDemo.Domain.Pets.Models;
 namespace ZerraDemo.Domain.Pets
 {
     [ServiceExposed]
-    [ServiceLog(BusLogging.HandlerOnly)]
     public interface IPetsQueryProvider
     {
         Task<ICollection<SpeciesModel>> GetSpecies();
         Task<ICollection<BreedModel>> GetBreeds(Guid speciesID);
         Task<ICollection<PetModel>> GetPets();
-
-        [ServiceLog(BusLogging.None)]
         Task<PetModel> GetPet(Guid id);
-
         Task<bool> IsHungry(Guid id);
         Task<bool> NeedsToPoop(Guid id);
     }
