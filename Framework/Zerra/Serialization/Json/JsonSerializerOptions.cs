601392e788e67eed83976806296cef401cbc4870
Steven Zawaski <5233295+szawaski@users.noreply.github.com>
2023-09-24 09:23:45 -0600

    documentation

--- Framework/Zerra/Serialization/Json/JsonSerializerOptions.cs
+++ Framework/Zerra/Serialization/Json/JsonSerializerOptions.cs
@@ -6,8 +6,17 @@ namespace Zerra.Serialization
 {
     public sealed class JsonSerializerOptions
     {
+        /// <summary>
+        /// A special feature to write JSON as arrays instead of property names. The exact same model is needed to deserialize.
+        /// </summary>
         public bool Nameless { get; set; }
+        /// <summary>
+        /// Properties with null values will not be written.
+        /// </summary>
         public bool DoNotWriteNullProperties { get; set; }
+        /// <summary>
+        /// Enums will serialize as their numeric value instead of the name string.
+        /// </summary>
         public bool EnumAsNumber { get; set; }
     }
 }
