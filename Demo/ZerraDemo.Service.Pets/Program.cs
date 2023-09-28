3106846b8247346b5a2baa77e22644b1046d3955
Steven Zawaski <5233295+szawaski@users.noreply.github.com>
2023-09-27 20:48:15 -0600

    Sockets

--- Demo/ZerraDemo.Service.Pets/Program.cs
+++ Demo/ZerraDemo.Service.Pets/Program.cs
@@ -10,7 +10,7 @@ namespace ZerraDemo.Service.Pets
         {
             Config.LoadConfiguration(args);
             ServiceManager.StartServices();
-            Bus.WaitForProcessExit();
+            Bus.WaitForExit();
         }
     }
 }
--- Demo/ZerraDemo.Service.Weather/Program.cs
+++ Demo/ZerraDemo.Service.Weather/Program.cs
@@ -10,7 +10,7 @@ namespace ZerraDemo.Service.Weather
         {
             Config.LoadConfiguration(args);
             ServiceManager.StartServices();
-            Bus.WaitForProcessExit();
+            Bus.WaitForExit();
         }
     }
 }
