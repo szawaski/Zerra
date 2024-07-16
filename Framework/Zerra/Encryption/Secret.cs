dded34b58110b3cc79b2e46b2bd6ceb5d8944169
Steven Zawaski <5233295+szawaski@users.noreply.github.com>
2024-07-15 23:15:42 -0600

    Secret Fixes

--- Framework/Zerra/Encryption/Secret.cs
+++ Framework/Zerra/Encryption/Secret.cs
@@ -9,9 +9,11 @@ namespace Zerra.Encryption
 {
     public sealed class Secret<T>
     {
+        [NonSerialized]
         private static readonly SymmetricKey key = SymmetricEncryptor.GenerateKey(SymmetricAlgorithmType.AES);
         [NonSerialized]
         private readonly byte[] secretEncrypted;
+
         public Secret(T secret)
         {
             var bytes = ByteSerializer.Serialize(secret);
@@ -27,48 +29,41 @@ namespace Zerra.Encryption
 
         public static bool operator ==(Secret<T> a, Secret<T> b)
         {
-            if (ReferenceEquals(a, null))
+            if (a is null)
             {
-                if (ReferenceEquals(b, null))
+                if (b is null)
                 {
                     return true;
                 }
                 else
                 {
-                    if (b.GetSecret() == null)
+                    if (b.GetSecret() is null)
                         return true;
                     else
                         return false;
                 }
             }
-            else if (ReferenceEquals(b, null))
+            else if (b is null)
             {
-                if (ReferenceEquals(a, null))
-                {
+                if (a.GetSecret() is null)
                     return true;
-                }
                 else
-                {
-                    if (a.GetSecret() == null)
-                        return true;
-                    else
-                        return false;
-                }
+                    return false;
             }
 
             var asecret = a.GetSecret();
-            if (asecret == null)
+            if (asecret is null)
             {
-                if (b.GetSecret() == null)
+                if (b.GetSecret() is null)
                     return true;
                 else
                     return false;
             }
 
             var bsecret = b.GetSecret();
-            if (bsecret == null)
+            if (bsecret is null)
             {
-                if (bsecret == null)
+                if (asecret is null)
                     return true;
                 else
                     return false;
@@ -77,100 +72,100 @@ namespace Zerra.Encryption
         }
         public static bool operator !=(Secret<T> a, Secret<T> b)
         {
-            if (ReferenceEquals(a, null))
+            if (a is null)
             {
-                if (ReferenceEquals(b, null))
+                if (b is null)
                 {
                     return false;
                 }
                 else
                 {
-                    if (b.GetSecret() == null)
+                    if (b.GetSecret() is null)
                         return false;
                     else
                         return true;
                 }
             }
-            else if (ReferenceEquals(b, null))
+            else if (b is null)
             {
-                if (a.GetSecret() == null)
+                if (a.GetSecret() is null)
                     return false;
                 else
                     return true;
             }
 
             var asecret = a.GetSecret();
-            if (asecret == null)
+            if (asecret is null)
             {
-                if (b.GetSecret() == null)
+                if (b.GetSecret() is null)
                     return false;
                 else
                     return true;
             }
 
             var bsecret = b.GetSecret();
-            if (bsecret == null)
+            if (bsecret is null)
             {
-                if (bsecret == null)
+                if (asecret is null)
                     return false;
                 else
                     return true;
             }
-            return asecret.Equals(bsecret);
+            return !asecret.Equals(bsecret);
         }
 
         public static bool operator ==(Secret<T> a, T b)
         {
-            if (ReferenceEquals(a, null))
+            if (a is null)
             {
-                if (ReferenceEquals(b, null))
+                if (b is null)
                     return true;
                 else
                     return false;
             }
-            else if (ReferenceEquals(b, null))
+            else if (b is null)
             {
-                if (a.GetSecret() == null)
+                if (a.GetSecret() is null)
                     return true;
                 else
                     return false;
             }
             var asecret = a.GetSecret();
-            if (asecret == null)
+            if (asecret is null)
                 return false;
             return asecret.Equals(b);
         }
         public static bool operator !=(Secret<T> a, T b)
         {
-            if (ReferenceEquals(a, null))
+            if (a is null)
             {
-                if (ReferenceEquals(b, null))
+                if (b is null)
                     return false;
                 else
                     return true;
             }
-            else if (ReferenceEquals(b, null))
+            else if (b is null)
             {
-                if (a.GetSecret() == null)
+                if (a.GetSecret() is null)
                     return false;
                 else
                     return true;
             }
             var asecret = a.GetSecret();
-            if (asecret == null)
+            if (asecret is null)
                 return true;
             return !asecret.Equals(b);
         }
 
         public static implicit operator T?(Secret<T>? it)
         {
-            if (ReferenceEquals(it, null))
+            if (it is null)
                 return default;
             return it.GetSecret();
         }
         public static explicit operator Secret<T>?(T? it)
         {
-            if (it == null)
+            if (it is null)
                 return null;
             return new Secret<T>(it);
         }
@@ -180,21 +175,26 @@ namespace Zerra.Encryption
             if (ReferenceEquals(this, obj))
                 return true;
 
-            if (ReferenceEquals(obj, null))
-                return false;
+            if (obj is null)
+            {
+                if (this.GetSecret() is null)
+                    return true;
+                else
+                    return false;
+            }
 
             if (obj is Secret<T> secretT)
             {
                 var secretTsecret = secretT.GetSecret();
                 var thissecret = this.GetSecret();
-                if (secretTsecret == null)
+                if (secretTsecret is null)
                 {
-                    if (thissecret == null)
+                    if (thissecret is null)
                         return true;
                     else
                         return false;
                 }
-                if (thissecret == null)
+                if (thissecret is null)
                     return false;
                 return secretTsecret.Equals(thissecret);
             }
@@ -202,14 +202,14 @@ namespace Zerra.Encryption
             if (obj is T t)
             {
                 var thissecret = this.GetSecret();
-                if (t == null)
+                if (t is null)
                 {
-                    if (thissecret == null)
+                    if (thissecret is null)
                         return true;
                     else
                         return false;
                 }
-                if (thissecret == null)
+                if (thissecret is null)
                     return false;
                 return t.Equals(thissecret);
             }
