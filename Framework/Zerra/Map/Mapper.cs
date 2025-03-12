b004a83db59d0f8788751d768a864dfdcb060691
Steven Zawaski <5233295+szawaski@users.noreply.github.com>
2025-03-12 16:42:49 -0600

    Map Overloads

--- Framework/Zerra/Map/Mapper.cs
+++ Framework/Zerra/Map/Mapper.cs
@@ -40,12 +40,14 @@ namespace Zerra.Map
 
         /// <summary>
         /// Populates the members of the target object from the source object.
+        /// Type is found from the generic parameter.
         /// Matching names are automatically mapped and will attempt to convert types.
         /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
         /// </summary>
         /// <typeparam name="TSource">The source type that will be mapped.</typeparam>
         /// <typeparam name="TTarget">The target type that will be mapped.</typeparam>
         /// <param name="source">The source object for the mapping.</param>
+        /// <param name="target">The target object for the properies.</param>
         /// <param name="graph">A graph to filter which properties to map.</param>
         public static void MapTo<TSource, TTarget>(this TSource source, TTarget target, Graph? graph = null)
         {
@@ -60,6 +62,7 @@ namespace Zerra.Map
 
         /// <summary>
         /// Creates a new object of the target type and populates the members from the source object.
+        /// Type is found using GetType.
         /// Matching names are automatically mapped and will attempt to convert types.
         /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
         /// </summary>
@@ -68,34 +71,6 @@ namespace Zerra.Map
         /// <param name="graph">A graph to filter which properties to map.</param>
         /// <returns>The new object of the target type with members populated from the source.</returns>
         public static TTarget Map<TTarget>(this object source, Graph? graph = null)
-        {
-            if (source is null)
-                throw new ArgumentNullException(nameof(source));
-
-            var sourceType = source.GetType();
-            var targetType = typeof(TTarget);
-
-            var key = new TypeKey(sourceType, targetType);
-            var copyFunc = copyFuncs.GetOrAdd(key, sourceType, targetType, static (sourceType, targetType) =>
-            {
-                var genericMapType = TypeAnalyzer.GetGenericTypeDetail(mapType, sourceType, targetType);
-                var map = genericMapType.GetMethodBoxed("GetMap").CallerBoxed(null, null);
-                return (s, g) => { return genericMapType.GetMethodBoxed("Copy").CallerBoxed(map, [s, g])!; };
-            });
-
-            return (TTarget)copyFunc(source, graph);
-        }
-
-        /// <summary>
-        /// Creates a copy of the source object populating the members.
-        /// Matching names are automatically mapped.
-        /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
-        /// </summary>
-        /// <typeparam name="TTarget">The type that will be copied.</typeparam>
-        /// <param name="source">The source object for the copy.</param>
-        /// <param name="graph">A graph to filter which properties to copy.</param>
-        /// <returns>The new object with members populated.</returns>
-        public static TTarget Copy<TTarget>(this TTarget source, Graph? graph = null)
         {
             if (source is null)
                 throw new ArgumentNullException(nameof(source));
@@ -116,13 +91,14 @@ namespace Zerra.Map
 
         /// <summary>
         /// Populates the members of the target object from the source object.
-        /// Matching names are automatically mapped.
+        /// Matching names are automatically mapped and will attempt to convert types.
         /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
         /// </summary>
-        /// <typeparam name="TTarget">The type that will be copied.</typeparam>
-        /// <param name="source">The source object for the copy.</param>
-        /// <param name="graph">A graph to filter which properties to copy.</param>
-        public static void CopyTo<TTarget>(this object source, TTarget target, Graph? graph = null)
+        /// <typeparam name="TTarget">The target type that will be mapped.</typeparam>
+        /// <param name="source">The source object for the mapping.</param>
+        /// <param name="target">The target object for the properies.</param>
+        /// <param name="graph">A graph to filter which properties to map.</param>
+        public static void MapTo<TTarget>(this object source, TTarget target, Graph? graph = null)
         {
             if (source is null)
                 throw new ArgumentNullException(nameof(source));
@@ -133,7 +109,7 @@ namespace Zerra.Map
             var targetType = typeof(TTarget);
 
             var key = new TypeKey(sourceType, targetType);
-            var copyToFunc = copyToFuncs.GetOrAdd(key, sourceType, targetType, static (sourceType, targetType) =>
+            var copyToFunc = copyToFuncs.GetOrAdd(key, sourceType, targetType, static (key, sourceType, targetType) =>
             {
                 var genericMapType = TypeAnalyzer.GetGenericTypeDetail(mapType, sourceType, targetType);
                 var map = genericMapType.GetMethodBoxed("GetMap").CallerBoxed(null, null);
@@ -141,6 +117,61 @@ namespace Zerra.Map
             });
 
             _ = copyToFunc(source, target, graph);
+        }
+
+        /// <summary>
+        /// Creates a copy of the source object populating the members.
+        /// Type is found from the generic parameter.
+        /// Matching names are automatically mapped.
+        /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
+        /// </summary>
+        /// <typeparam name="TTarget">The type that will be copied.</typeparam>
+        /// <param name="source">The source object for the copy.</param>
+        /// <param name="graph">A graph to filter which properties to copy.</param>
+        /// <returns>The new object with members populated.</returns>
+        public static TTarget Copy<TTarget>(this TTarget source, Graph? graph = null)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            var type = typeof(TTarget);
+
+            var key = new TypeKey(type, type);
+            var copyFunc = copyFuncs.GetOrAdd(key, type, type, static (key, sourceType, targetType) =>
+            {
+                var genericMapType = TypeAnalyzer.GetGenericTypeDetail(mapType, sourceType, targetType);
+                var map = genericMapType.GetMethodBoxed("GetMap").CallerBoxed(null, null);
+                return (s, g) => { return genericMapType.GetMethodBoxed("Copy").CallerBoxed(map, [s, g])!; };
+            });
+
+            return (TTarget)copyFunc(source, graph);
+        }
+
+        /// <summary>
+        /// Creates a copy of the source object populating the members.
+        /// Type is found using GetType.
+        /// Matching names are automatically mapped.
+        /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
+        /// </summary>
+        /// <param name="source">The source object for the copy.</param>
+        /// <param name="graph">A graph to filter which properties to copy.</param>
+        /// <returns>The new object with members populated.</returns>
+        public static object Copy(object source, Graph? graph = null)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            var type = source.GetType();
+
+            var key = new TypeKey(type, type);
+            var copyFunc = copyFuncs.GetOrAdd(key, type, type, static (key, sourceType, targetType) =>
+            {
+                var genericMapType = TypeAnalyzer.GetGenericTypeDetail(mapType, sourceType, targetType);
+                var map = genericMapType.GetMethodBoxed("GetMap").CallerBoxed(null, null);
+                return (s, g) => { return genericMapType.GetMethodBoxed("Copy").CallerBoxed(map, [s, g])!; };
+            });
+
+            return copyFunc(source, graph);
         }
     }
 }
--- Framework/Zerra/Map/MapperWithLog.cs
+++ Framework/Zerra/Map/MapperWithLog.cs
@@ -13,7 +13,7 @@ namespace Zerra.Map
     /// </summary>
     public static class MapperWithLog
     {
-        internal static bool DebugMode { get; set; } = false;
+        internal static bool DebugMode { get; set; } = Config.IsDebugBuild;
 
         private static readonly Type mapType = typeof(MapGeneratorWithLog<,>);
         private static readonly ConcurrentFactoryDictionary<TypeKey, Func<object, IMapLogger, Graph?, object>> copyFuncs = new();
@@ -21,6 +21,7 @@ namespace Zerra.Map
 
         /// <summary>
         /// Creates a new object of the target type and populates the members from the source object.
+        /// Type is found from the generic parameter.
         /// Matching names are automatically mapped and will attempt to convert types.
         /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
         /// </summary>
@@ -41,12 +42,14 @@ namespace Zerra.Map
 
         /// <summary>
         /// Populates the members of the target object from the source object.
+        /// Type is found from the generic parameter.
         /// Matching names are automatically mapped and will attempt to convert types.
         /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
         /// </summary>
         /// <typeparam name="TSource">The source type that will be mapped.</typeparam>
         /// <typeparam name="TTarget">The target type that will be mapped.</typeparam>
         /// <param name="source">The source object for the mapping.</param>
+        /// <param name="target">The target object for the properies.</param>
         /// <param name="logger">A logger instance to record the changes.</param>
         /// <param name="graph">A graph to filter which properties to map.</param>
         public static void MapTo<TSource, TTarget>(this TSource source, TTarget target, IMapLogger logger, Graph? graph = null)
@@ -62,6 +65,7 @@ namespace Zerra.Map
 
         /// <summary>
         /// Creates a new object of the target type and populates the members from the source object.
+        /// Type is found using GetType.
         /// Matching names are automatically mapped and will attempt to convert types.
         /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
         /// </summary>
@@ -90,44 +94,16 @@ namespace Zerra.Map
         }
 
         /// <summary>
-        /// Creates a copy of the source object populating the members.
-        /// Matching names are automatically mapped.
-        /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
-        /// </summary>
-        /// <typeparam name="TTarget">The type that will be copied.</typeparam>
-        /// <param name="source">The source object for the copy.</param>
-        /// <param name="logger">A logger instance to record the changes.</param>
-        /// <param name="graph">A graph to filter which properties to copy.</param>
-        /// <returns>The new object with members populated.</returns>
-        public static TTarget Copy<TTarget>(this TTarget source, IMapLogger logger, Graph? graph = null)
-        {
-            if (source is null)
-                throw new ArgumentNullException(nameof(source));
-
-            var sourceType = source.GetType();
-            var targetType = typeof(TTarget);
-
-            var key = new TypeKey(sourceType, targetType);
-            var copyFunc = copyFuncs.GetOrAdd(key, sourceType, targetType, static (sourceType, targetType) =>
-            {
-                var genericMapType = TypeAnalyzer.GetGenericTypeDetail(mapType, sourceType, targetType);
-                var map = genericMapType.GetMethodBoxed("GetMap").CallerBoxed(null, null);
-                return (s, l, g) => { return genericMapType.GetMethodBoxed("Copy").CallerBoxed(map, [s, l, g])!; };
-            });
-
-            return (TTarget)copyFunc(source, logger, graph);
-        }
-
-        /// <summary>
         /// Populates the members of the target object from the source object.
-        /// Matching names are automatically mapped.
+        /// Matching names are automatically mapped and will attempt to convert types.
         /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
         /// </summary>
-        /// <typeparam name="TTarget">The type that will be copied.</typeparam>
-        /// <param name="source">The source object for the copy.</param>
+        /// <typeparam name="TTarget">The target type that will be mapped.</typeparam>
+        /// <param name="source">The source object for the mapping.</param>
+        /// <param name="target">The target object for the properies.</param>
         /// <param name="logger">A logger instance to record the changes.</param>
-        /// <param name="graph">A graph to filter which properties to copy.</param>
-        public static void CopyTo<TTarget>(this object source, TTarget target, IMapLogger logger, Graph? graph = null)
+        /// <param name="graph">A graph to filter which properties to map.</param>
+        public static void MapTo<TTarget>(this object source, TTarget target, IMapLogger logger, Graph? graph = null)
         {
             if (source is null)
                 throw new ArgumentNullException(nameof(source));
@@ -146,6 +122,63 @@ namespace Zerra.Map
             });
 
             _ = copyToFunc(source, target, logger, graph);
+        }
+
+        /// <summary>
+        /// Creates a copy of the source object populating the members.
+        /// Type is found from the generic parameter.
+        /// Matching names are automatically mapped.
+        /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
+        /// </summary>
+        /// <typeparam name="TTarget">The type that will be copied.</typeparam>
+        /// <param name="source">The source object for the copy.</param>
+        /// <param name="logger">A logger instance to record the changes.</param>
+        /// <param name="graph">A graph to filter which properties to copy.</param>
+        /// <returns>The new object with members populated.</returns>
+        public static TTarget Copy<TTarget>(this TTarget source, IMapLogger logger, Graph? graph = null)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            var type = typeof(TTarget);
+
+            var key = new TypeKey(type, type);
+            var copyFunc = copyFuncs.GetOrAdd(key, type, type, static (sourceType, targetType) =>
+            {
+                var genericMapType = TypeAnalyzer.GetGenericTypeDetail(mapType, sourceType, targetType);
+                var map = genericMapType.GetMethodBoxed("GetMap").CallerBoxed(null, null);
+                return (s, l, g) => { return genericMapType.GetMethodBoxed("Copy").CallerBoxed(map, [s, l, g])!; };
+            });
+
+            return (TTarget)copyFunc(source, logger, graph);
+        }
+
+        /// <summary>
+        /// Creates a copy of the source object populating the members.
+        /// Type is found using GetType.
+        /// Matching names are automatically mapped.
+        /// Customizations are made by implementing <see cref="IMapDefinition{TSource, TTarget}"/>
+        /// </summary>
+        /// <param name="source">The source object for the copy.</param>
+        /// <param name="logger">A logger instance to record the changes.</param>
+        /// <param name="graph">A graph to filter which properties to copy.</param>
+        /// <returns>The new object with members populated.</returns>
+        public static object Copy(this object source, IMapLogger logger, Graph? graph = null)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            var type = source.GetType();
+
+            var key = new TypeKey(type, type);
+            var copyFunc = copyFuncs.GetOrAdd(key, type, type, static (sourceType, targetType) =>
+            {
+                var genericMapType = TypeAnalyzer.GetGenericTypeDetail(mapType, sourceType, targetType);
+                var map = genericMapType.GetMethodBoxed("GetMap").CallerBoxed(null, null);
+                return (s, l, g) => { return genericMapType.GetMethodBoxed("Copy").CallerBoxed(map, [s, l, g])!; };
+            });
+
+            return copyFunc(source, logger, graph);
         }
     }
 }
