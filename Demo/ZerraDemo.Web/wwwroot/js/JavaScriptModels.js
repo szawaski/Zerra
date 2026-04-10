2ca5c211f607a73453430d78730d947ce1c5e55b
Steven Zawaski <5233295+szawaski@users.noreply.github.com>
2026-04-09 21:52:08 -0600

    Version Up

--- Demo/ZerraDemo.Web/wwwroot/js/JavaScriptModels.js
+++ Demo/ZerraDemo.Web/wwwroot/js/JavaScriptModels.js
@@ -1,74 +1,3 @@
-const Balance1ModelType =
-{
-    AccountID: "string",
-    Balance: "number",
-    LastTransactionDate: "Date",
-    LastTransactionAmount: "number",
-}
-
-const Transaction1ModelType =
-{
-    AccountID: "string",
-    Date: "Date",
-    Amount: "number",
-    Description: "string",
-    Balance: "number",
-    Event: "string",
-}
-
-const Balance2ModelType =
-{
-    AccountID: "string",
-    Balance: "number",
-    LastTransactionDate: "Date",
-    LastTransactionAmount: "number",
-}
-
-const Transaction2ModelType =
-{
-    AccountID: "string",
-    Date: "Date",
-    Amount: "number",
-    Description: "string",
-    Balance: "number",
-    Event: "string",
-}
-
-const SpeciesModelType =
-{
-    ID: "string",
-    Name: "string",
-}
-
-const BreedModelType =
-{
-    ID: "string",
-    Name: "string",
-}
-
-const PetModelType =
-{
-    ID: "string",
-    Name: "string",
-    Breed: "string",
-    Species: "string",
-    LastEaten: "Date",
-    AmountEaten: "number",
-    LastPooped: "Date",
-}
-
-const WeatherModelType =
-{
-    Date: "Date",
-    WeatherType: "string",
-}
-
-const WeatherCachedModelType =
-{
-    Date: "Date",
-    WeatherType: "string",
-}
-
 const FeedPetCommandResultType =
 {
     AmountEaten: "number",
@@ -76,89 +5,14 @@ const FeedPetCommandResultType =
 
 const ModelTypeDictionary =
 {
-    Balance1Model: Balance1ModelType,
-    Transaction1Model: Transaction1ModelType,
-    Balance2Model: Balance2ModelType,
-    Transaction2Model: Transaction2ModelType,
-    SpeciesModel: SpeciesModelType,
-    BreedModel: BreedModelType,
-    PetModel: PetModelType,
-    WeatherModel: WeatherModelType,
-    WeatherCachedModel: WeatherCachedModelType,
     FeedPetCommandResult: FeedPetCommandResultType,
-}
-
-const ILedger1QueryProvider = {
-    HasBalance: function(accountID, onComplete, onFail) {
-        Bus.Call("ILedger1QueryProvider", "HasBalance", [accountID], null, false, onComplete, onFail);
-    },
-    GetBalance: function(accountID, onComplete, onFail) {
-        Bus.Call("ILedger1QueryProvider", "GetBalance", [accountID], Balance1ModelType, false, onComplete, onFail);
-    },
-    GetTransactions: function(accountID, onComplete, onFail) {
-        Bus.Call("ILedger1QueryProvider", "GetTransactions", [accountID], Transaction1ModelType, true, onComplete, onFail);
-    },
-}
-
-const ILedger2QueryProvider = {
-    HasBalance: function(accountID, onComplete, onFail) {
-        Bus.Call("ILedger2QueryProvider", "HasBalance", [accountID], null, false, onComplete, onFail);
-    },
-    GetBalance: function(accountID, onComplete, onFail) {
-        Bus.Call("ILedger2QueryProvider", "GetBalance", [accountID], Balance2ModelType, false, onComplete, onFail);
-    },
-    GetTransactions: function(accountID, onComplete, onFail) {
-        Bus.Call("ILedger2QueryProvider", "GetTransactions", [accountID], Transaction2ModelType, true, onComplete, onFail);
-    },
-}
-
-const IPetsQueryProvider = {
-    GetSpecies: function(onComplete, onFail) {
-        Bus.Call("IPetsQueryProvider", "GetSpecies", [], SpeciesModelType, true, onComplete, onFail);
-    },
-    GetBreeds: function(speciesID, onComplete, onFail) {
-        Bus.Call("IPetsQueryProvider", "GetBreeds", [speciesID], BreedModelType, true, onComplete, onFail);
-    },
-    GetPets: function(onComplete, onFail) {
-        Bus.Call("IPetsQueryProvider", "GetPets", [], PetModelType, true, onComplete, onFail);
-    },
-    GetPet: function(id, onComplete, onFail) {
-        Bus.Call("IPetsQueryProvider", "GetPet", [id], PetModelType, false, onComplete, onFail);
-    },
-    IsHungry: function(id, onComplete, onFail) {
-        Bus.Call("IPetsQueryProvider", "IsHungry", [id], null, false, onComplete, onFail);
-    },
-    NeedsToPoop: function(id, onComplete, onFail) {
-        Bus.Call("IPetsQueryProvider", "NeedsToPoop", [id], null, false, onComplete, onFail);
-    },
-}
-
-const IWeatherQueryProvider = {
-    GetWeather: function(onComplete, onFail) {
-        Bus.Call("IWeatherQueryProvider", "GetWeather", [], WeatherModelType, false, onComplete, onFail);
-    },
-    GetWeatherSync: function(cancellationToken, onComplete, onFail) {
-        Bus.Call("IWeatherQueryProvider", "GetWeatherSync", [cancellationToken], WeatherModelType, false, onComplete, onFail);
-    },
-    TestStreams: function(onComplete, onFail) {
-        Bus.Call("IWeatherQueryProvider", "TestStreams", [], null, false, onComplete, onFail);
-    },
-}
-
-const IWeatherCachedQueryProvider = {
-    GetWeather: function(onComplete, onFail) {
-        Bus.Call("IWeatherCachedQueryProvider", "GetWeather", [], WeatherCachedModelType, false, onComplete, onFail);
-    },
-    TestStreams: function(onComplete, onFail) {
-        Bus.Call("IWeatherCachedQueryProvider", "TestStreams", [], null, false, onComplete, onFail);
-    },
 }
 
 const Deposit1Command = function(properties) {
     this.AccountID = (properties === undefined || properties.AccountID === undefined) ? null : properties.AccountID;
     this.Amount = (properties === undefined || properties.Amount === undefined) ? null : properties.Amount;
     this.Description = (properties === undefined || properties.Description === undefined) ? null : properties.Description;
-    this.CommandType = "Deposit1Command";
+    this.CommandType = "ZerraDemo.Domain.Ledger1.Command.Deposit1Command";
     this.CommandWithResult = false;
     this.ResultType = null;
     this.ResultTypeHasMany = false;
@@ -169,7 +23,7 @@ const Transfer1Command = function(properties) {
     this.ToAccountID = (properties === undefined || properties.ToAccountID === undefined) ? null : properties.ToAccountID;
     this.Amount = (properties === undefined || properties.Amount === undefined) ? null : properties.Amount;
     this.Description = (properties === undefined || properties.Description === undefined) ? null : properties.Description;
-    this.CommandType = "Transfer1Command";
+    this.CommandType = "ZerraDemo.Domain.Ledger1.Command.Transfer1Command";
     this.CommandWithResult = false;
     this.ResultType = null;
     this.ResultTypeHasMany = false;
@@ -179,7 +33,7 @@ const Withdraw1Command = function(properties) {
     this.AccountID = (properties === undefined || properties.AccountID === undefined) ? null : properties.AccountID;
     this.Amount = (properties === undefined || properties.Amount === undefined) ? null : properties.Amount;
     this.Description = (properties === undefined || properties.Description === undefined) ? null : properties.Description;
-    this.CommandType = "Withdraw1Command";
+    this.CommandType = "ZerraDemo.Domain.Ledger1.Command.Withdraw1Command";
     this.CommandWithResult = false;
     this.ResultType = null;
     this.ResultTypeHasMany = false;
@@ -189,7 +43,7 @@ const Deposit2Command = function(properties) {
     this.AccountID = (properties === undefined || properties.AccountID === undefined) ? null : properties.AccountID;
     this.Amount = (properties === undefined || properties.Amount === undefined) ? null : properties.Amount;
     this.Description = (properties === undefined || properties.Description === undefined) ? null : properties.Description;
-    this.CommandType = "Deposit2Command";
+    this.CommandType = "ZerraDemo.Domain.Ledger2.Command.Deposit2Command";
     this.CommandWithResult = false;
     this.ResultType = null;
     this.ResultTypeHasMany = false;
@@ -200,7 +54,7 @@ const Transfer2Command = function(properties) {
     this.ToAccountID = (properties === undefined || properties.ToAccountID === undefined) ? null : properties.ToAccountID;
     this.Amount = (properties === undefined || properties.Amount === undefined) ? null : properties.Amount;
     this.Description = (properties === undefined || properties.Description === undefined) ? null : properties.Description;
-    this.CommandType = "Transfer2Command";
+    this.CommandType = "ZerraDemo.Domain.Ledger2.Command.Transfer2Command";
     this.CommandWithResult = false;
     this.ResultType = null;
     this.ResultTypeHasMany = false;
@@ -210,7 +64,7 @@ const Withdraw2Command = function(properties) {
     this.AccountID = (properties === undefined || properties.AccountID === undefined) ? null : properties.AccountID;
     this.Amount = (properties === undefined || properties.Amount === undefined) ? null : properties.Amount;
     this.Description = (properties === undefined || properties.Description === undefined) ? null : properties.Description;
-    this.CommandType = "Withdraw2Command";
+    this.CommandType = "ZerraDemo.Domain.Ledger2.Command.Withdraw2Command";
     this.CommandWithResult = false;
     this.ResultType = null;
     this.ResultTypeHasMany = false;
@@ -220,7 +74,7 @@ const AdoptPetCommand = function(properties) {
     this.PetID = (properties === undefined || properties.PetID === undefined) ? null : properties.PetID;
     this.BreedID = (properties === undefined || properties.BreedID === undefined) ? null : properties.BreedID;
     this.Name = (properties === undefined || properties.Name === undefined) ? null : properties.Name;
-    this.CommandType = "AdoptPetCommand";
+    this.CommandType = "ZerraDemo.Domain.Pets.Commands.AdoptPetCommand";
     this.CommandWithResult = false;
     this.ResultType = null;
     this.ResultTypeHasMany = false;
@@ -229,7 +83,7 @@ const AdoptPetCommand = function(properties) {
 const FeedPetCommand = function(properties) {
     this.PetID = (properties === undefined || properties.PetID === undefined) ? null : properties.PetID;
     this.Amount = (properties === undefined || properties.Amount === undefined) ? null : properties.Amount;
-    this.CommandType = "FeedPetCommand";
+    this.CommandType = "ZerraDemo.Domain.Pets.Commands.FeedPetCommand";
     this.CommandWithResult = true;
     this.ResultType = FeedPetCommandResultType;
     this.ResultTypeHasMany = false;
@@ -237,7 +91,7 @@ const FeedPetCommand = function(properties) {
 
 const LetPetOutToPoopCommand = function(properties) {
     this.PetID = (properties === undefined || properties.PetID === undefined) ? null : properties.PetID;
-    this.CommandType = "LetPetOutToPoopCommand";
+    this.CommandType = "ZerraDemo.Domain.Pets.Commands.LetPetOutToPoopCommand";
     this.CommandWithResult = false;
     this.ResultType = null;
     this.ResultTypeHasMany = false;
@@ -245,7 +99,7 @@ const LetPetOutToPoopCommand = function(properties) {
 
 const SetWeatherCommand = function(properties) {
     this.WeatherType = (properties === undefined || properties.WeatherType === undefined) ? null : properties.WeatherType;
-    this.CommandType = "SetWeatherCommand";
+    this.CommandType = "ZerraDemo.Domain.Weather.Commands.SetWeatherCommand";
     this.CommandWithResult = false;
     this.ResultType = null;
     this.ResultTypeHasMany = false;
@@ -253,7 +107,7 @@ const SetWeatherCommand = function(properties) {
 
 const SetWeatherCachedCommand = function(properties) {
     this.WeatherType = (properties === undefined || properties.WeatherType === undefined) ? null : properties.WeatherType;
-    this.CommandType = "SetWeatherCachedCommand";
+    this.CommandType = "ZerraDemo.Domain.WeatherCached.Commands.SetWeatherCachedCommand";
     this.CommandWithResult = false;
     this.ResultType = null;
     this.ResultTypeHasMany = false;
